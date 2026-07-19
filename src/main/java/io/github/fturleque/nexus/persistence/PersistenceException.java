3510989956665d9640f0cb15437049ff205344f6
Turleque Fabrice <57303912+FTurleque@users.noreply.github.com>
2026-07-19 20:28:11 +0200

    feat: ajouter l'exception de persistance NEXUS

---
+++ src/main/java/io/github/fturleque/nexus/persistence/PersistenceException.java
@@ -0,0 +1,8 @@
+package io.github.fturleque.nexus.persistence;
+
+public final class PersistenceException extends RuntimeException {
+
+    public PersistenceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
