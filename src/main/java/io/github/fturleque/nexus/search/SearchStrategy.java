5e070509e187ecdba76d1f99dbfb52bc86e21773
Turleque Fabrice <57303912+FTurleque@users.noreply.github.com>
2026-07-19 21:40:24 +0200

    feat(search): permettre les stratégies de recherche IO

--- src/main/java/io/github/fturleque/nexus/search/SearchStrategy.java
+++ src/main/java/io/github/fturleque/nexus/search/SearchStrategy.java
@@ -2,9 +2,10 @@ package io.github.fturleque.nexus.search;
 
 import io.github.fturleque.nexus.project.ProjectDescriptor;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface SearchStrategy {
 
-    List<SearchCandidate> search(ProjectDescriptor project, String query, int limit);
+    List<SearchCandidate> search(ProjectDescriptor project, String query, int limit) throws IOException;
 }
